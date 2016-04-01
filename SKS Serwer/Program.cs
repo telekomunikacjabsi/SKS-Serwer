08d85c491e3fc6cae8e79d3ea55cb0cbd45c606c
Przemek <ebp7@protonmail.com>
2016-04-01 11:52:00 +0200

    Add project files.

---
+++ SKS Serwer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKS_Serwer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
